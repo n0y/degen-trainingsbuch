7164bbd44d7320398c00f4c615efe401ebf82920
Bertram Fey <bertram.fey@corelogics.de>
2025-05-27 13:03:46 +0200

    Texte überarbeitet, Quellen verlinkt

--- src/lookup-publication.sh
+++ src/lookup-publication.sh
@@ -2,5 +2,6 @@
 
 # Pubmed "PMID", "PMCID" mit:          "pmid:<ID>"
 # DOI einfach mit der DOI als String:  "<ID>"
+# mit ISB einfach als string "<ID>"
 
 npx citation-js --plugins isbn,doi,pubmed -f string -s bibtex -t "$1"
